a75b82d12095718c58e4513f4a4453291ef15897
Costath <thalesdaniel@gmail.com>
2020-05-24 22:48:18 -0400

    fixed event handler for text changed

--- src/App.js
+++ src/App.js
@@ -14,13 +14,19 @@ class App extends Component {
     showPersons: false
   }
   
-  nameChangedHandler = (event) => {
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => p.id === id)
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [ ...this.state.persons];
+    persons[personIndex] = person;
+
     this.setState({
-      persons: [
-        { name: 'Thales', age: 27},
-        { name: event.target.value, age: 28},
-        { name: 'Elisa', age: 3}
-      ]
+      persons: persons
     })
   }
 
@@ -55,7 +61,8 @@ class App extends Component {
               click={() => this.deletePersonHandler(index)}
               name={person.name} 
               age={person.age}
-              key={person.id} />
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)} />
           })}
         </div>
       )
